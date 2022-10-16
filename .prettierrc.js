b6e73478d58b201a3eb805016c935e60f24a992a
Issi <issi.dev.iobroker@gmail.com>
2022-10-16 23:01:51 +0200

    update files to new Version

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,17 +1,28 @@
+// module.exports = {
+// 	semi: true,
+// 	trailingComma: 'all',
+// 	singleQuote: true,
+// 	printWidth: 160,
+// 	max_line_length: 160,
+// 	useTabs: true,
+// 	tabWidth: 4,
+// 	endOfLine: 'lf',
+// 	insert_final_newline: true,
+// 	overrides: [
+// 		{
+// 			files: 'admin/words.js',
+// 			options: {
+// 				useTabs: false,
+// 			},
+// 		},
+// 	],
+// };
 module.exports = {
 	semi: true,
 	trailingComma: 'all',
 	singleQuote: true,
 	printWidth: 120,
-	useTabs: true,
+	useTabs: false,
 	tabWidth: 4,
 	endOfLine: 'lf',
-	overrides: [
-		{
-			files: 'admin/words.js',
-			options: {
-				useTabs: false,
-			},
-		},
-	],
 };
